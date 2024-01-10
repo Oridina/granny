a1b3ee90a573f89b6c57fad32b126a3ed3ff61bd
Oridina <58023257+Oridina@users.noreply.github.com>
2024-01-10 08:03:37 +0700

    - import assets 2 + TMPro + Cinemachine

---
+++ Assets/Scripts/Game/SceneMainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneMainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void Setting()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
